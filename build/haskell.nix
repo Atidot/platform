9d7498dc1ae22d78e9e81c883b74a4a9e7d0ff1a
Barak Bercovitz <barakber@gmail.com>
2019-10-17 21:08:47 +0300

    build: static initially works

--- build/haskell.nix
+++ build/haskell.nix
@@ -24,13 +24,13 @@ let
   platformVisualSrc  = ../platform-visual;
 
   projectPackages = hspkgs: {
-    stratosphere     = hspkgs.callCabal2nix "stratosphere"    "${stratosphereSrc}" {};
-    platform-types   = hspkgs.callCabal2nix "platform-types"  "${platformTypesSrc}" {};
-    platform-dsl     = hspkgs.callCabal2nix "platform-dsl"    "${platformDSLSrc}" {};
-    platform-aws     = hspkgs.callCabal2nix "platform-aws"    "${platformAWSSrc}" {};
-    platform-kube    = hspkgs.callCabal2nix "platform-kube"   "${platformKubeSrc}" {};
+    stratosphere     = hspkgs.callCabal2nix "stratosphere"     "${stratosphereSrc}" {};
+    platform-types   = hspkgs.callCabal2nix "platform-types"   "${platformTypesSrc}" {};
+    platform-dsl     = hspkgs.callCabal2nix "platform-dsl"     "${platformDSLSrc}" {};
+    platform-aws     = hspkgs.callCabal2nix "platform-aws"     "${platformAWSSrc}" {};
+    platform-kube    = hspkgs.callCabal2nix "platform-kube"    "${platformKubeSrc}" {};
     platform-process = hspkgs.callCabal2nix "platform-process" "${platformProcessSrc}" {};
-    platform-visual  = hspkgs.callCabal2nix "platform-visual" "${platformVisualSrc}" {};
+    platform-visual  = hspkgs.callCabal2nix "platform-visual"  "${platformVisualSrc}" {};
   };
 in
 haskellPackages.override (old: {
