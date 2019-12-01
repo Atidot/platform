41e9cae6f13b52e8850231f3b0f09c3e46bc3890
TalZ <tal.z@atidot.com>
2019-12-01 11:12:06 +0200

    Terraform.hs: more progression

--- platform-terraform/src/Atidot/Platform/Terraform.hs
+++ platform-terraform/src/Atidot/Platform/Terraform.hs
@@ -1,6 +1,5 @@
 module Atidot.Platform.Terraform where
 
-
 import "base"                   Control.Monad(void)
 import "base"                   Data.Traversable(for)
 import "base"                   Data.Monoid
@@ -12,15 +11,27 @@ import qualified "containers"   Data.Map as M
 import qualified "text"         Data.Text as T
 import qualified "terraform-hs" Language.Terraform.Util.Text as T
 
-simpleConfig :: TF ()
-simpleConfig = do
-  inst <- awsInstance' "atidot-instance" "ami-2757f631" "t2.micro"
-  awsVpc "atidot-vpc" "10.0.0.0/16" $ \vpcParams -> vpcParams{_vpc_enable_dns_support = True, _vpc_enable_dns_hostnames = True}
-  awsEip "atidot-ip" $ \eipParams -> eipParams{_eip_vpc = True, _eip_instance = Just $ (i_id inst)}
+
+simpleConfig :: T.Text -> TF ()
+simpleConfig zone = do
+  instId <- awsInstance' "atidot-instance" "ami-2757f631" "t2.micro"
+  awsVpc <- awsVpc "atidot-vpc" "10.0.0.0/16" $ \vpcParams -> vpcParams{_vpc_enable_dns_support = True, _vpc_enable_dns_hostnames = True}
+  awsEip "atidot-env" $ \eipParams -> eipParams{_eip_vpc = True, _eip_instance = Just $ (i_id instId)}
+  awsInternetGateway' "atidot-env-gw" (vpc_id awsVpc)
+  awsSn <- awsSubnet "atidot-subnet" (vpc_id awsVpc) "${cidrsubnet(aws_vpc.atidot-env.cidr_block, 3, 1)}" $ \sbParams -> sbParams{_sn_availability_zone = zone} -- <-- replace here after implementation
+  awsRt <- awsRouteTable "atidot-rt" (vpc_id awsVpc) $ \rtParams -> rtParams{_rt_tags = ("cidr_block" =: "0.0.0.0/0") <> ("gateway_id" =: "${aws_internet_gateway.atidot-env-gw.id")}  -- add route entry
+  awsRouteTableAssociation' "atidot-env-assoc" (sn_id awsSn) (rt_id awsRt)
   return ()
+
+(=:) k v = M.singleton k v
+
+-- to be added declaratively and replace the comment in line 20
+cidrSubnet :: AwsId AwsSubnet -> Int -> Int -> String
+cidrSubnet = undefined
 
 mkDep :: IO ()
 mkDep = generateFiles "" $
-  withNameScope "example" $ do
-    newAws (makeAwsParams "us-east-1"){aws_profile="default"}
-    simpleConfig
+  let zone = "us-east-1"
+  in withNameScope "example" $ do
+    newAws (makeAwsParams zone){aws_profile="default"}
+    simpleConfig zone
