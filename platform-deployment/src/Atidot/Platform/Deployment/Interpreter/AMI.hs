d1684f90062f6f0a56da722ae0059a2c77efbe37
TalZ <tal.z@atidot.com>
2019-12-10 14:07:36 +0200

    AMI.hs: added working ssh example

--- platform-deployment/src/Atidot/Platform/Deployment/Interpreter/AMI.hs
+++ platform-deployment/src/Atidot/Platform/Deployment/Interpreter/AMI.hs
@@ -1,6 +1,7 @@
 module Atidot.Platform.Deployment.Interpreter.AMI where
 
 import                        Prelude hiding (FilePath)
+import           "base"       Data.List
 import           "text"       Data.Text (Text)
 import qualified "text"       Data.Text as T
 import qualified "text"       Data.Text.IO as T
@@ -8,11 +9,14 @@ import           "free"       Control.Monad.Free
 import           "mtl"        Control.Monad.State
 import           "exceptions" Control.Monad.Catch (MonadMask, bracket)
 import           "turtle"     Turtle
+import           "simplessh"  Network.SSH.Client.SimpleSSH
 
 import                        Atidot.Platform.Deployment
 import                        Atidot.Platform.Deployment.Interpreter.AMI.Template
 import                        Atidot.Platform.Deployment.Interpreter.AMI.Types
+import Debug.Trace
 
+lttrace x y = trace (x ++ ":" ++ show y) y
 -- // to login
 -- // $ ssh ubuntu@<public_dns> -i ~/.ssh/terraform-keys2
 
@@ -24,7 +28,7 @@ runAMI config dep =
     where
         terraformDepDir = "terraform_dep" :: FilePath
         terraformAwsDep = renderProvider config allTemplates
-        getPublicDns = undefined
+        getPublicDns = T.takeWhile (/= '"') . T.tail . T.dropWhile (/= '"') . snd . T.breakOn "public_dns"
         reduceShell =  reduce $ Fold (<>) "" lineToText
         init' :: IO Text
         init' = do
@@ -34,7 +38,7 @@ runAMI config dep =
             procs "terraform" ["init"] stdin
             procs "terraform" ["apply", "-auto-approve"] stdin
             applyOutput <- reduceShell $ inproc "terraform" ["show"] stdin
-            let publicDns = applyOutput
+            let publicDns = getPublicDns applyOutput
             return publicDns
         fini :: Text -> IO ()
         fini publicDns = do
@@ -43,7 +47,27 @@ runAMI config dep =
             procs "terraform" ["destroy"] stdin
             cd ".."
             return ()
+        body :: Text -> IO ()
         body publicDns = do
+            let [knownHostFp,pubkey,privkey] = map ("~/.ssh/" <>) ["known_hosts","terraform-keys2.pub","terraform-keys2"]
+                port = 22
+                hostname = T.unpack publicDns
+            putStrLn "------------------------------------------"
+            putStrLn hostname
+            sshW publicDns ["touch","hello"]
+            --session <- liftIO $ runSimpleSSH $ do
+                --session <- openSession hostname port knownHostFp
+                --session' <- authenticateWithKey session "ubuntu" pubkey privkey ""
+                --output "~/hello" "hello"
+                --execCommand session' "touch ~/hello"
+                -- withSessionKey hostname port knownHostFp "ubuntu" pubkey privkey "" $ \sess -> execCommand sess "touch ~/hello"
+                --return session
+            --    return ()
+            --auth_session <- authenticateWithPasswordSource session username password
+            -- session'' <- case runErrorT session' of
+                -- Left simple_ssh_error -> error "break stuff" {- SimpleSSHError -}
+                -- Right sess -> return session
+
             _ <-  (runStateT (iterM (run publicDns) dep) config)
             return ()
 
@@ -60,3 +84,6 @@ runAMI config dep =
         run _ (Mount disk volume next) = do
             liftIO $ putStrLn "some storage mount"
             next True
+
+
+sshW pdns cmd = procs "ssh" (["ubuntu@"<>pdns,"-o","StrictHostKeyChecking=no","-i","~/.ssh/terraform-keys2"] ++ cmd) stdin
