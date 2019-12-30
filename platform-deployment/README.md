46fadf280b0ada037fafeac701dcdfd0abb68026
TalZ <tal.z@atidot.com>
2019-12-30 14:10:19 +0200

    platform-deployment: added CLI and added some documentation

--- platform-deployment/README.md
+++ platform-deployment/README.md
@@ -1,1 +1,45 @@
 # platform-deployment
+
+## intro
+
+allows us to declare a deployment inside an aws instance, with dockers, disks and secrets declarative management
+
+## example script
+
+``` haskell
+nsss :: DeploymentM Bool
+nsss = do
+    secret <- secret "tutorials/MyFirstTutorialSecret"
+    dir <- mount "data"
+    b <- container "hello-world"
+    attachSecret secret "hello-world"
+    attachVolume dir "hello-world"
+    execute [] "hello-world" []
+    return b
+```
+
+This script Declares:
+
+1. A secret
+2. An external disk "data"
+3. a container named hello world
+4. attaches the secret to the container
+5. attaches the mount to the container
+6. executes the container
+
+## How to Run
+
+``` bash
+$ cd ../build
+..
+$ make shell
+... # in the newly opened nix shell
+$ platform-deployment-exe --script nsss
+{initializes terraform and generates files from example}
+$ cd terraform_dep
+...
+$ ls
+aws_cli_config.tf  example.tf
+$ terraform apply # applies genetated configuration
+...
+```
--- platform-deployment/app/Main.hs
+++ platform-deployment/app/Main.hs
@@ -1,9 +1,30 @@
+{-# LANGUAGE DeriveGeneric     #-}
 module Main where
 
-import "data-default" Data.Default
-import                Atidot.Platform.Deployment
-import                Atidot.Platform.Deployment.Interpreter.Terraform
-import                Atidot.Platform.Deployment.Interpreter.AMI.Types
+import "optparse-generic" Options.Generic
+import "data-default"     Data.Default
+import                    Atidot.Platform.Deployment
+import                    Atidot.Platform.Deployment.Interpreter.Terraform
+import                    Atidot.Platform.Deployment.Interpreter.AMI.Types
+
+data CLI = CLI
+    {script :: String
+    } deriving (Generic, Show)
+
+instance ParseRecord CLI
+
 
 main :: IO ()
-main = runTerraform def nsss --kiss
+main = do
+    x <- script <$> getRecord "Platform Deployment"
+    case lookup x scripts of
+        Just s -> runTerraform def s
+        Nothing -> error $ unlines
+            [ "script '" ++ x ++ "' not found"
+            , "available scripts are: " ++ show (map fst scripts)
+            ]
+
+scripts =
+    [ ("nsss",nsss)
+    , ("kiss",kiss)
+    ]
