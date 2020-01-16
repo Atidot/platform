2c28bb9f4474eb72147fc5a87f469da3fb0758ee
TalZ <tal.z@atidot.com>
2020-01-16 16:14:23 +0200

    Revert "platform-deployment: deleted second executable"
    This reverts commit a9b02c50b5ac99157a094200f17aa7447a24ae61.

---
+++ platform-deployment/app/PkgDep.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE DeriveGeneric     #-}
+module PkgDep where
+
+import "optparse-generic" Options.Generic
+import "data-default"     Data.Default
+import                    Atidot.Platform.Deployment
+import                    Atidot.Platform.Deployment.Interpreter.Terraform
+import                    Atidot.Platform.Deployment.Interpreter.AMI.Types
+
+data CLI = CLI
+    {script :: String
+    } deriving (Generic, Show)
+
+instance ParseRecord CLI
+
+
+main :: IO ()
+main = do
+    x <- script <$> getRecord "Platform Deployment"
+    case lookup x scripts of
+        Just s -> runTerraform def s
+        Nothing -> error $ unlines
+            [ "script '" ++ x ++ "' not found"
+            , "available scripts are: " ++ show (map fst scripts)
+            ]
+
+scripts =
+    [ ("nsss",nsss)
+    , ("kiss",kiss)
+    , ("nes",noneExistentSecret)
+    , ("nesa",noneExistentSecretAttached)
+    , ("sdt", secretDeclaredTwice)
+    , ("sat",secretAttachedTwice)
+    , ("cdne",containerDoesNotExists)
+    ]
--- scripts/packaging-deployment.sh
+++ scripts/packaging-deployment.sh
@@ -6,7 +6,7 @@ declare -i index=1
 for var in "$@"
 do
     platform-packaging pytodocker --input $var --outfile $DEPLOYMENT/$var
-    platform-deployment -s someScript # change this to work with platform-deployment
+    packaging-deployment _ --pythondir $var --dockerifle $DEPLOYMENT/$var # change this to work with platform-deployment
 done
 
 rm -rf "$DEPLOYMENT"
